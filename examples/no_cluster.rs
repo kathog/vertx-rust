f797728ba1c1e66e9e5e3de36c7dca3fe47b4c50
Daniel Lewiński <nerull@szwadron.net>
2022-06-25 08:30:42 +0200

    change parking_lot version

--- examples/no_cluster.rs
+++ examples/no_cluster.rs
@@ -1,12 +1,12 @@
 #![feature(async_closure)]
-use crossbeam_channel::{bounded};
+use crossbeam_channel::bounded;
 use hyper::Response;
 use hyper::StatusCode;
 use vertx_rust::http::client::WebClient;
-use vertx_rust::vertx::{cm::NoClusterManager, Vertx, VertxOptions};
 use vertx_rust::vertx::message::Body;
+use vertx_rust::vertx::{cm::NoClusterManager, Vertx, VertxOptions};
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 12)]
 async fn main() {
     pretty_env_logger::init_timed();
     console_subscriber::init();
@@ -21,42 +21,35 @@ async fn main() {
 
     event_bus.consumer("test.01", move |m, _| {
         let b = m.body();
-        let response = format!(
-            r#"{{"health": "{code}"}}"#,
-            code = b.as_i32().unwrap()
-        );
+        let response = format!(r#"{{"health": "{code}"}}"#, code = b.as_i32().unwrap());
         m.reply(Body::ByteArray(response.into_bytes()));
     });
     let net_server = vertx.create_net_server();
-    net_server.listen(9091, move |_req, ev| {
-        let mut resp = vec![];
-        let (tx, rx) = bounded(1);
-            ev.request(
-                "test.01",
-                Body::Int(101),
-                move |m, _| {
-                    let _ = tx.send(m.body());
-                },
-            );
+    net_server
+        .listen(9091, move |_req, ev| {
+            let mut resp = vec![];
+            let (tx, rx) = bounded(1);
+            ev.request("test.01", Body::Int(101), move |m, _| {
+                let _ = tx.send(m.body());
+            });
 
-                let body = rx.recv().unwrap();
-                let body = body.as_bytes().unwrap();
-                let data = format!(
-                    r#"
+            let body = rx.recv().unwrap();
+            let body = body.as_bytes().unwrap();
+            let data = format!(
+                r#"
 HTTP/1.1 200 OK
 content-type: application/json
 Date: Sun, 03 May 2020 07:05:15 GMT
 Content-Length: {len}
 
 {json_body}"#,
-                    len = body.len(),
-                    json_body = String::from_utf8_lossy(body)
-                );
-                resp.extend_from_slice(data.as_bytes());
-                resp
-
-
-    }).await;
+                len = body.len(),
+                json_body = String::from_utf8_lossy(body)
+            );
+            resp.extend_from_slice(data.as_bytes());
+            resp
+        })
+        .await;
 
     let mut http_server = vertx.create_http_server();
     http_server
